6b2348c065606b556b654936b07c80948c91bbf4
Jonathan Yu <jonathan@motherknows.com>
2013-04-14 17:41:43 -0700

    Cleans up Stuff + Adds bastardized scrolling
    like, really bastardized scrolling

--- app/controllers/heskyize_view_controller.rb
+++ app/controllers/heskyize_view_controller.rb
@@ -11,25 +11,35 @@ class HeskyizeViewController < UIViewController
     self.navigationItem.rightBarButtonItem = UIBarButtonItem.titled('Translate') {openTranslate}
     # You'll need to create another UINavigationController, push your modal_controller into it, and present that
     
-    header_frame = view.bounds.down(10).width(view.bounds.width).height(60)
+    scroll_frame = view.bounds
+    scroll_view = UIScrollView.alloc.initWithFrame scroll_frame
+    scroll_view.contentSize = CGSizeMake(scroll_frame.width, scroll_frame.height*2)
+    scroll_view.scrollEnabled = true
+    view << scroll_view
+    # 
+    # subframe = view.bounds
+    # subframe_view = UIView.alloc.initWithFrame subframe
+    # scroll_view << subframe_view
+    
+    header_frame = view.bounds.down(10).width(view.bounds.width).height(70)
     header_label = UITextView.alloc.initWithFrame header_frame
     header_label.text = "In the text box below, what text do you want to translate?"
     header_label.editable = false
     header_label.font = :bold.uifont(20)
     header_label.textColor = UIColor.whiteColor
     header_label.backgroundColor = :clear.uicolor
-    view << header_label 
+    scroll_view << header_label 
     
-    edit_frame = header_frame.below(10).width(view.bounds.width).height(view.bounds.height)
-    @edit_label = UITextView.alloc.initWithFrame edit_frame
+    edit_frame = view.bounds.down(80).width(view.bounds.width).height(view.bounds.height)
+    subview = UIView.alloc.initWithFrame edit_frame
+    scroll_view << subview
+    @edit_label = UITextView.alloc.initWithFrame(edit_frame.below(5).width(300).centered_in(edit_frame))
     @edit_label.editable = true
     @edit_label.backgroundColor = "subtle_white_feathers.png".uicolor
     @edit_label.font = :bold.uifont(16)
-    # edit_label.on :editing_did_end do
-    #   @inputtext = edit_label.text
-    # end
-    # edit_label.off :end
-    view << @edit_label
+    @edit_label.scrollEnabled = true
+    
+    subview << @edit_label
 
     
   end
